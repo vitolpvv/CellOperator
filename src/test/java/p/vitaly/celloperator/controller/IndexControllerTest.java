33818b801c4fcf9112f46e0e6177c7f7cfe6758a
VitalyP <vitol_pvv@mail.ru>
2019-07-26 01:57:47 +0300

    Add first test

---
+++ src/test/java/p/vitaly/celloperator/controller/IndexControllerTest.java
@@ -0,0 +1,42 @@
+package p.vitaly.celloperator.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration("../../../../../webapp/WEB-INF/web.xml")
+@ContextConfiguration(locations = {"classpath:dispatcher-servlet.xml"})
+public class IndexControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void servletContextProvidesIndexController() {
+        ServletContext servletContext = wac.getServletContext();
+
+        assertNotNull(servletContext);
+        assertTrue(servletContext instanceof MockServletContext);
+        assertNotNull(wac.getBean("indexController"));
+    }
+}
